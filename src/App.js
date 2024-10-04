81730a55d1e022ace1fbfa26cb17ab6166304fad
Rajpal@123 <you@example.com>
2024-10-04 10:45:44 +0530

    JSX syntax & component re-usability

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ function Square(params) {
+   return <button className="square">{params}</button>
 }
+
+// first or general approach.
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className="board-row">
+//         <button className="square">1</button>
+//         <button className="square">2</button>
+//         <button className="square">3</button>
+//       </div>
+//       <div className="board-row">
+//         <button className="square">4</button>
+//         <button className="square">5</button>
+//         <button className="square">6</button>
+//       </div>
+//       <div className="board-row">
+//         <button className="square">7</button>
+//         <button className="square">8</button>
+//         <button className="square">9</button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// component re-usable feature of react.
+const App = () => {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+      <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+      <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+};
 
 export default App;
